1ed48173f02ee570972e88fa76488ec139b6d14c
Armando Salazar <armandosalazar351@gmail.com>
2024-01-24 15:07:20 -0600

    feat: add get post by id using @RequestParam like query params

--- src/main/java/org/armandosalazar/component/BasicController.java
+++ src/main/java/org/armandosalazar/component/BasicController.java
@@ -4,6 +4,7 @@ import org.armandosalazar.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
@@ -14,7 +15,7 @@ public class BasicController {
 
     @GetMapping(path = {"/index"})
     public String index() {
-        return "index";
+        return "home";
     }
 
     @GetMapping(path = {"/home", "/hello", "/gallery"})
@@ -28,6 +29,13 @@ public class BasicController {
         return "index";
     }
 
+    @GetMapping(path = {"/post"})
+    public ModelAndView post(@RequestParam(defaultValue = "1", name = "id", required = false) int id) {
+        ModelAndView modelAndView = new ModelAndView("post");
+        this.getPosts().stream().filter(post -> post.getId() == id).forEach(post -> modelAndView.addObject("post", post));
+        return modelAndView;
+    }
+
     @GetMapping(path = "/public")
     public ModelAndView publicPage() {
         ModelAndView modelAndView = new ModelAndView("index");
@@ -37,10 +45,10 @@ public class BasicController {
 
     public List<Post> getPosts() {
         List<Post> posts = new ArrayList<>();
-        posts.add(new Post(1, "Web Development", "A web developer is a specialist who has the necessary knowledge for the creation and maintenance of websites. In general terms, the web developer is in charge of designing the sites and developing their functions, using programming languages and specialized software.", "/img/post.png", "https://picsum.photos/200/300"));
-        posts.add(new Post(2, "Web Development Front-End", "A web developer is a specialist who has the necessary knowledge for the creation and maintenance of websites. In general terms, the web developer is in charge of designing the sites and developing their functions, using programming languages and specialized software.", "/img/post.png", "https://picsum.photos/200/300"));
-        posts.add(new Post(3, "Web Development Back-End", "A web developer is a specialist who has the necessary knowledge for the creation and maintenance of websites. In general terms, the web developer is in charge of designing the sites and developing their functions, using programming languages and specialized software.", "/img/post.png", "https://picsum.photos/200/300"));
-        posts.add(new Post(4, "Web Development UX/UI", "A web developer is a specialist who has the necessary knowledge for the creation and maintenance of websites. In general terms, the web developer is in charge of designing the sites and developing their functions, using programming languages and specialized software.", "/img/post.png", "https://picsum.photos/200/300"));
+        posts.add(new Post(1, "Web Development", "A web developer is a specialist who has the necessary knowledge for the creation and maintenance of websites. In general terms, the web developer is in charge of designing the sites and developing their functions, using programming languages and specialized software.", "/img/post.png", "/post?id=1"));
+        posts.add(new Post(2, "Web Development Front-End", "A web developer is a specialist who has the necessary knowledge for the creation and maintenance of websites. In general terms, the web developer is in charge of designing the sites and developing their functions, using programming languages and specialized software.", "/img/post.png", "/post?id=2"));
+        posts.add(new Post(3, "Web Development Back-End", "A web developer is a specialist who has the necessary knowledge for the creation and maintenance of websites. In general terms, the web developer is in charge of designing the sites and developing their functions, using programming languages and specialized software.", "/img/post.png", "/post?id=3"));
+        posts.add(new Post(4, "Web Development UX/UI", "A web developer is a specialist who has the necessary knowledge for the creation and maintenance of websites. In general terms, the web developer is in charge of designing the sites and developing their functions, using programming languages and specialized software.", "/img/post.png", "/post?id=4"));
         return posts;
     }
 
